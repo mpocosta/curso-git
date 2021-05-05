0fe2073667c833c0129829012d5ee8f7b5fa9c8d
Marcos Costa <mpocosta@hotmail.com>
2021-05-05 20:20:51 -0300

    Foi adicionada a frase como voce estah ?

--- Primeiro/Program.cs
+++ Primeiro/Program.cs
@@ -7,7 +7,7 @@ namespace Primeiro
         static void Main(string[] args)
         {
             Console.WriteLine("Olá, Mundo!");
-
+            Console.WriteLine("Como você está ?");
             Console.WriteLine("Bom dia !!!");
         }
     }
